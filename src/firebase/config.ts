446cdcfe900f8bebd79f87090b4c73df3472280b
Luis Felipe Tavera Orozco <felipetavera0412@gmail.com>
2025-10-02 13:26:03 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-553323312-cd93e",
+  "appId": "1:751195907889:web:fcd3165691f6772639b210",
+  "apiKey": "AIzaSyCd3mC5cuPSi-XIGGqCyY7_sGWrdf46P9E",
+  "authDomain": "studio-553323312-cd93e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "751195907889"
+};
