ae4f1be3364e168f4daa7605a250112a9e1117e2
luistaveraflyr <luis.tavera@newshore.es>
2025-10-16 08:17:57 -0500

    fix: update metadata structure to enhance SEO and social sharing capabilities

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ import { AuthProvider } from "@/context/auth-context";
 import "./globals.css";
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://taveralabs.com"),
   title: {
     template: "%s | Luis Felipe Tavera Orozco",
     default: "Home | Luis Felipe Tavera Orozco", // Fallback title
@@ -29,8 +30,21 @@ export const metadata: Metadata = {
     url: "https://taveralabs.com",
     siteName: "Luis Felipe Tavera Orozco",
     images: [{ url: "/assets/Logo.png" }],
-    type: "website"
-  }
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Luis Felipe Tavera Orozco - Software Engineer",
+    description:
+      "Luis Felipe Tavera Orozco portfolio, Software Engineer with more than 8 years of experience. Expert in developing scalable applications with .Net, React, Angular, clean architectures, and SOLID principles.",
+    images: ["/assets/Logo.png"],
+    creator: "@taveralabs",
+  },
+  icons: {
+    icon: "/assets/Logo.png",
+    shortcut: "/assets/Logo.png",
+    apple: "/assets/Logo.png",
+  },
 };
 
 export default function RootLayout({
