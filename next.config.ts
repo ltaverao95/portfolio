202a3c2edb3d19f921bb2c7bf148563c79700a0a
luistaveraflyr <luis.tavera@newshore.es>
2025-12-04 07:54:21 -0500

    chore: Remove `eslint.ignoreDuringBuilds` configuration from Next.js.

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone', // Required for Docker deployment
   typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
